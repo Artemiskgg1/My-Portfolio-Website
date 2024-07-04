3668a1a5de150b2ad9ac72637a4c9c3db858bf76
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-07-04 20:32:33 +0530

    feat: Add Hero component to display spotlight in the landing page

---
+++ app/components/Hero.tsx
@@ -0,0 +1,14 @@
+import { Spotlight } from "@/components/ui/spotlight";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36 ">
+      <div>
+        <Spotlight />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
