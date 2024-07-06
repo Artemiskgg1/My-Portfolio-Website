e94875a26c6304be9e706b089238a3b025715e2e
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-07-06 23:08:35 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,9 +29,12 @@ export default function RootLayout({
           />
           <Spotlight
             className="top-10 left-full h-[80vh] w-[50vh]"
-            fill="purple"
+            fill="#9f5ea5"
           />
-          <Spotlight className="top-28 left-80 h-[80vh] w-[50vh]" fill="blue" />
+          <Spotlight
+            className="top-28 left-80 h-[80vh] w-[50vh]"
+            fill="#4b6382"
+          />
         </div>
         <div className="h-screen w-full bg-black-100  bg-grid-white/[0.03]  flex items-center justify-center absolute top-0 left-0">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
