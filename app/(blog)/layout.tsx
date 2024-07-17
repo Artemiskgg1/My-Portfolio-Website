4ae6f594fa7cfab753c56ccb14070a7412858b3a
Abha Ghildiyal <100835504+AbhaGhildiyal@users.noreply.github.com>
2024-07-17 23:25:01 +0530

    Refactor BlogLayout component for improved responsiveness and layout consistency

--- app/(blog)/layout.tsx
+++ app/(blog)/layout.tsx
@@ -2,21 +2,6 @@
 
 import LeftSection from "./blog-page/_components/left-section";
 
-// export default function BlogLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   return (
-//     <div className="flex flex-col lg:flex-row items-stretch justify-center mt-[7rem]">
-//       <div className="w-full lg:w-1/4 p-5">
-//         <LeftSection />
-//       </div>
-//       <div className="flex-1 p-5 flex justify-center">{children}</div>
-//     </div>
-//   );
-// }
-
 export default function BlogLayout({
   children,
 }: {
