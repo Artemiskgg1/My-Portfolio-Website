68e77813d3ee8c8c94942b79c2345a95ccbd8a77
iynes <abhaghildiyal@gmail.com>
2024-08-27 23:18:46 +0530

    fixed github commit code

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
             fill="#4b6382"
           />
         </div>
-        <div className="h-screen w-full bg-black-100  bg-grid-white/[0.03]  flex items-center justify-center absolute top-0 left-0">
+        <div className=" w-full bg-black-100  bg-grid-white/[0.03]  flex items-center justify-center absolute top-0 left-0">
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100  [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
         </div>
         <Navbar />
