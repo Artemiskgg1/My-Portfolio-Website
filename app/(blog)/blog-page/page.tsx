42c51e37831285921b4a52f7670db7641eb745a9
iynes <abhaghildiyal@gmail.com>
2024-09-05 21:57:18 +0530

    Refactor blog page layout for improved responsiveness

--- app/(blog)/blog-page/page.tsx
+++ app/(blog)/blog-page/page.tsx
@@ -2,7 +2,7 @@ import MidPage from "./_components/mid-page";
 
 export default function BlogPage() {
   return (
-    <div className="md:h-screen flex md:w-full bg-black-100/50">
+    <div className="md:h-screen flex md:w-full">
       <div className="flex justify-center md:mt-[5.5rem] mr-10">
         <MidPage />
       </div>
